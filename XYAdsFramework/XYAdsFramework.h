5951f88dc8d9125361558284cd357b2903f02b05
Xiangyu Zhao <5624130asdz>
2018-03-15 14:50:20 +0800

    Initial Commit

---
+++ XYAdsFramework/XYAdsFramework.h
@@ -0,0 +1,19 @@
+//
+//  XYAdsFramework.h
+//  XYAdsFramework
+//
+//  Created by zhaoxiangyu on 2018/3/15.
+//  Copyright © 2018年 xiangyu.zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XYAdsFramework.
+FOUNDATION_EXPORT double XYAdsFrameworkVersionNumber;
+
+//! Project version string for XYAdsFramework.
+FOUNDATION_EXPORT const unsigned char XYAdsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XYAdsFramework/PublicHeader.h>
+
+
